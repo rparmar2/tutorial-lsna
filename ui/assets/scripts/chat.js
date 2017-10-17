1205136e63a9b495fea00bab1a4484184831e5f6
rparmar2 <rparmar@coach.com>
2017-10-17 10:51:59 -0400

    Update chat.js

--- ui/assets/scripts/chat.js
+++ ui/assets/scripts/chat.js
@@ -136,5 +136,33 @@ function animateMessage(message) {
  */
 
 // Paste Watch New Messages code below //
+function userCreate() {
+  WeDeploy
+    .auth(address.auth)
+    .createUser({
+      name: create.name.value,
+      email: create.email.value,
+      password: create.password.value,
+      color: 'color-' + Math.floor(Math.random() * 19)
+    })
+    .then(function() {
+      create.submit.disabled = true;
+      create.submit.innerText = 'Loading...';
 
+      WeDeploy
+        .auth(address.auth)
+        .signInWithEmailAndPassword(create.email.value, create.password.value)
+        .then(function() {
+          document.location.href = './chat.html';
+        })
+        .catch(function() {
+          alert('Sign-in failed.');
+        });
+    })
+    .catch(function() {
+      create.submit.disabled = false;
+      create.submit.innerText = 'Create Account';
+      alert('Sign-up failed.');
+    })
+};
 // Paste Watch New Messages code above //
