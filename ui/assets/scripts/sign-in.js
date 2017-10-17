5e20101c608632d9b41565a7873607c28eff9ff6
rparmar2 <rparmar@coach.com>
2017-10-17 10:50:10 -0400

    Update sign-in.js

--- ui/assets/scripts/sign-in.js
+++ ui/assets/scripts/sign-in.js
@@ -12,5 +12,19 @@ if (WeDeploy.auth(address.auth).currentUser) {document.location.href = './chat.h
 var login = document.querySelector('.login');
 
 // Paste Sign In code below //
-
+function signIn() {
+  WeDeploy
+    .auth(address.auth)
+    .signInWithEmailAndPassword(login.email.value, login.password.value)
+    .then(function() {
+      login.submit.disabled = true;
+      login.submit.innerText = 'Loading...';
+      document.location.href = './chat.html';
+    })
+    .catch(function() {
+      login.submit.disabled = false;
+      login.submit.innerText = 'Sign In';
+      alert('Sign-in failed.');
+    });
+}
 // Paste Sign In code above //
